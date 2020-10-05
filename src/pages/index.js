ded85974ccea48d0ffb4e04706c85f6ab235897a
Mohamed El Omari <Med-ELOMARI@users.noreply.github.com>
2020-10-05 19:47:46 +0200

    test 1

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,6 +22,7 @@ const IndexPage = ({
         <title>{site.siteMetadata.title}</title>
         <meta name="description" content={site.siteMetadata.description} />
         <script>
+        {`
           console.log('hi there');
           (function() {
 window.__insp = window.__insp || [];
@@ -30,6 +31,7 @@ var ldinsp = function(){
 if(typeof window.__inspld != "undefined") return; window.__inspld = 1; var insp = document.createElement('script'); insp.type = 'text/javascript'; insp.async = true; insp.id = "inspsync"; insp.src = ('https:' == document.location.protocol ? 'https' : 'http') + '://cdn.inspectlet.com/inspectlet.js?wid=322612993&r=' + Math.floor(new Date().getTime()/3600000); var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(insp, x); };
 setTimeout(ldinsp, 0);
 })();
+`}
         </script>
       </Helmet>
       <HeroHeader/>
