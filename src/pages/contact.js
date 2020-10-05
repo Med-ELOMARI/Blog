5e57389b7f5131131466276547828f077de32d6e
Mohamed El Omari <Med-ELOMARI@users.noreply.github.com>
2020-10-05 19:26:06 +0200

    inspect let

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -13,15 +13,6 @@ const ContactPage = ({
       <Helmet>
         <title>Contact — {site.siteMetadata.title}</title>
         <meta name="description" content={"Contact page of " + site.siteMetadata.description} />
-        <script type="text/javascript">
-(function() {
-window.__insp = window.__insp || [];
-__insp.push(['wid', 322612993]);
-var ldinsp = function(){
-if(typeof window.__inspld != "undefined") return; window.__inspld = 1; var insp = document.createElement('script'); insp.type = 'text/javascript'; insp.async = true; insp.id = "inspsync"; insp.src = ('https:' == document.location.protocol ? 'https' : 'http') + '://cdn.inspectlet.com/inspectlet.js?wid=322612993&r=' + Math.floor(new Date().getTime()/3600000); var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(insp, x); };
-setTimeout(ldinsp, 0);
-})();
-</script>
       </Helmet>
       <div className="two-grids -contact">
         <div className="post-thumbnail" style={{backgroundImage: `url('/assets/alexander-andrews-HgUDpaGPTEA-unsplash.jpg')`, marginBottom: 0}}>
--- src/pages/index.js
+++ src/pages/index.js
@@ -21,15 +21,6 @@ const IndexPage = ({
       <Helmet>
         <title>{site.siteMetadata.title}</title>
         <meta name="description" content={site.siteMetadata.description} />
-        <script type="text/javascript">
-(function() {
-window.__insp = window.__insp || [];
-__insp.push(['wid', 322612993]);
-var ldinsp = function(){
-if(typeof window.__inspld != "undefined") return; window.__inspld = 1; var insp = document.createElement('script'); insp.type = 'text/javascript'; insp.async = true; insp.id = "inspsync"; insp.src = ('https:' == document.location.protocol ? 'https' : 'http') + '://cdn.inspectlet.com/inspectlet.js?wid=322612993&r=' + Math.floor(new Date().getTime()/3600000); var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(insp, x); };
-setTimeout(ldinsp, 0);
-})();
-</script>
       </Helmet>
       <HeroHeader/>
       <h2>Blog Posts &darr;</h2>
